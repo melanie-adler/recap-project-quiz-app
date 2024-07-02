321a0239ef7b117184d27ea88c8af0b6a7ba21db
Melanie Adler <adl.melanie.95@gmail.com>
2024-07-02 12:55:56 +0200

    add 'show-answer' to all buttons

--- src/index.js
+++ src/index.js
@@ -1,75 +1,30 @@
-const answerButton = document.querySelector('[data-js="answer-button"]');
-const answer = document.querySelector('[data-js="answer"]');
-const bookmark = document.querySelector('[data-js="bookmark-button"]');
-console.log(bookmark);
-const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
-console.log(bookmarkIcon);
+const answerButton = document.querySelectorAll('[data-js="answer-button"]');
+const answer = document.querySelectorAll('[data-js="answer"]');
 
-answerButton.addEventListener("click", () => {
-  showAnswer();
+answerButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    showAnswer(button, answer[index]);
+  });
 });
 
-function showAnswer() {
+function showAnswer(button, answer) {
   answer.classList.toggle("show-answer");
-  answerButton.textContent === "Hide Answer"
-    ? (answerButton.textContent = "Show Answer")
-    : (answerButton.textContent = "Hide Answer");
+  answerButton.forEach((p) => {
+    button.textContent =
+      button.textContent === "Hide Answer" ? "Show Answer" : "Hide Answer";
+  });
 }
+
+const bookmark = document.querySelector('[data-js="bookmark-button"]');
+const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
 
 bookmark.addEventListener("click", () => {
   bookmarkQuestion();
 });
 
 function bookmarkQuestion() {
+  const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
   bookmarkIcon.src === "/src/icons/bookmark-fill-black.svg"
     ? (bookmarkIcon.src = "/src/icons/bookmark-fill-white.svg")
     : (bookmarkIcon.src = "/src/icons/bookmark-fill-black.svg");
 }
-
-// apply to all buttons
-
-// const answerButton = document.querySelectorAll('[data-js="answer-button"]');
-// const answer = document.querySelectorAll('[data-js="answer"]');
-
-// answerButton.forEach((answerButton) => {
-//   answerButton.addEventListener("click", () => {
-//     console.log("Hello");
-
-//     answerButton.textContent === "Hide Answer"
-//       ? (answerButton.textContent = "Show Answer")
-//       : (answerButton.textContent = "Hide Answer");
-
-//     // answer.forEach((answer) => {
-//     //   answer.classList.toggle("show-answer");
-//     //   answerButton.textContent === "Hide Answer"
-//     //     ? (answerButton.textContent = "Show Answer")
-//     //     : (answerButton.textContent = "Hide Answer");
-//     // });
-//   });
-// });
-
-// const bookmark = document.querySelector('[data-js="bookmark-button"]');
-// console.log(bookmark);
-// const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
-// console.log(bookmarkIcon);
-
-// answerButton.addEventListener("click", () => {
-//   showAnswer();
-// });
-
-// function showAnswer() {
-//   answer.classList.toggle("show-answer");
-//   answerButton.textContent === "Hide Answer"
-//     ? (answerButton.textContent = "Show Answer")
-//     : (answerButton.textContent = "Hide Answer");
-// }
-
-// bookmark.addEventListener("click", () => {
-//   bookmarkQuestion();
-// });
-
-// function bookmarkQuestion() {
-//   bookmarkIcon.src === "./src/icons/bookmark-fill-black.svg"
-//     ? (bookmarkIcon.src = "./src/icons/bookmark-fill-white.svg")
-//     : (bookmarkIcon.src = "./src/icons/bookmark-fill-black.svg");
-// }
